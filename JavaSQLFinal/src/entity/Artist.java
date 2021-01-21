41c2b9a88a4d820cd5bddb2bf924332531de9825
Lisa <las619@comcast.net>
2021-01-21 13:58:30 -0500

    check in artistDao & artist

---
+++ JavaSQLFinal/src/entity/Artist.java
@@ -0,0 +1,39 @@
+package entity;
+
+public class Artist {
+	private  int artistId;
+	private String artistName;
+	
+	/*
+	 * Constructor
+	 */
+	
+	public Artist (int artistId, String artistName) {
+		this.artistId = artistId;
+		this.artistName = artistName;
+	}
+
+	
+	/*
+	 * Getters & Setters
+	 */
+	
+	public int getArtistId() {
+		return artistId;
+	}
+
+	public void setArtistId(int artistId) {
+		this.artistId = artistId;
+	}
+
+	public String getArtistName() {
+		return artistName;
+	}
+
+	public void setArtistName(String artistName) {
+		this.artistName = artistName;
+	}
+	
+	
+	
+}
